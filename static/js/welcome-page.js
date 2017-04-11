3744d479a6563e4c6818b0977ce9d0f470088e3a
nathandh@ndh-ubuntu.thrifthound <nathandhernandez@gmail.com>
2017-04-10 23:41:03 -0400

    Save POINT for working Welcome Page Tabs to display BOTH Individual User Blog posts, and ALL Users Blog Posts

--- static/js/welcome-page.js
+++ static/js/welcome-page.js
@@ -4,6 +4,75 @@ function makeWelcomeTabs(){
 	var welcomeTabDIV = document.getElementsByClassName("welcome-tabs")[0];
 	console.log("Received welcome tab element" + welcomeTabDIV);
 
+	var firstAnchor = null;
+	var secondAnchor = null;
+
+	var myPostsDIV = document.getElementById("my-posts");
+	var allPostsDIV = document.getElementById("all-posts");
+	
+	var tabChildren = welcomeTabDIV.childNodes;
+	console.log(tabChildren.length);
+	for (var i = 0; i < tabChildren.length; i++){
+		var currTag = tabChildren[i];
+		console.log("Examining: " + currTag.nodeName);
+		
+		if (currTag.nodeName === "UL"){
+			var myList = currTag;
+			firstAnchor = currTag.firstChild.nextElementSibling.firstChild;
+			secondAnchor = currTag.firstChild.nextElementSibling.nextElementSibling.firstChild;
+			console.log(firstAnchor);
+			console.log(secondAnchor);
+			break;
+		}
+		/*
+		if (tabChildren[i].classList.contains("active")){
+			console.log(tabChildre[i]);
+		}*/
+	}
+
+	/* Add Click Listeners */
+	var tabAnchors = [firstAnchor, secondAnchor];
+	for (var i = 0; i < tabAnchors.length; i++){
+		tabAnchors[i].addEventListener("click", function(e){
+			console.log(this.innerHTML);
+			if (!this.classList.contains("active")){
+				// Add 'active' class if doesn't exist
+				this.classList.add("active");
+
+				if (this.innerHTML === "MY posts"){
+					console.log("After click MyPosts tab is ACTIVE");
+					allPostsDIV.style.display = "none";
+					myPostsDIV.style.display ="block";
+
+					secondAnchor.classList.remove("active");
+				} else if (this.innerHTML === "ALL posts"){
+					console.log("After click AllPosts tab is ACTIVE");
+					myPostsDIV.style.display = "none";
+					allPostsDIV.style.display = "block";
+
+					firstAnchor.classList.remove("active");
+				}
+
+				this.style.display = "block";
+			}
+		});
+	}
+
+	if (firstAnchor.classList.contains("active")){
+		console.log("MyPosts tab is ACTIVE");
+		firstAnchor.style.display = "block";
+
+		// Hide the AllPosts DIV
+		allPostsDIV.style.display = "none";
+	} else if (secondAnchor.classList.contains("active")){
+		console.log("AllPosts tab is ACTIVE");
+		secondAnchor.style.display = "block";
+
+		// Hide MyPosts
+		myPostsDIV.style.display = "none";
+	} else {
+		console.log("ERROR: No Tabs are marked as ACTIVE!");
+	}
 }
 
 // Document Ready Listener
